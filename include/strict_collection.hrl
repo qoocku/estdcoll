1e93e48150624aca106125120b435ab679b49cf4
Damian Dobroczy\\'nski <qoocku@gmail.com>
2011-03-17 15:55:13 +0100

    strict collection introduced

--- include/strict_collection.hrl
+++ include/strict_collection.hrl
@@ -1,6 +1,7 @@
 -ifndef (ESTDCOLL_RANDOM_ACCESS_COLLECTION_HRL).
 -define (ESTDCOLL_RANDOM_ACCESS_COLLECTION_HRL, true).
 
+-define (COLLECTION_BEHAVIOR_TYPES, true).
 -include_lib ("estdcoll/include/collection.hrl").
 
 -ifdef (RANDOM_ACCESS_COLLECTION_BEHAVIOR_EXPORTS).
